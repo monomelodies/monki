507971e047dab5d67113bd11eff68b19e1624c2d
Marijn Ophorst <marijn@sensimedia.nl>
2015-10-02 08:36:42 +0200

    update tests

--- tests/Monki/ApiTest.php
+++ tests/Monki/ApiTest.php
@@ -13,11 +13,13 @@ class MonkiApiTest extends AbstractTest
         $api = new Monki\Api($db, $router);
         $api->browse();
         $state = $router->resolve('/foo/');
-        $found = json_decode("$state", true);
+        $found = json_decode($state(), true);
         $this->assertEquals(4, count($found));
         $_POST = ['action' => 'create', 'data' => ['content' => 'whee']];
+        $_SERVER['REQUEST_METHOD'] = 'POST';
         $state = $router->resolve('/foo/', 'POST');
-        $found = json_decode("$state", true);
+        $found = json_decode($state(), true);
+        $_SERVER['REQUEST_METHOD'] = 'GET';
         $this->assertEquals('whee', $found['content']);
     }
 
@@ -31,7 +33,7 @@ class MonkiApiTest extends AbstractTest
         $api = new Monki\Api($db, $router);
         $api->count();
         $state = $router->resolve('/foo/count/');
-        $found = json_decode("$state", true);
+        $found = json_decode($state(), true);
         $this->assertEquals(4, $found['count']);
     }
 
@@ -48,17 +50,19 @@ class MonkiApiTest extends AbstractTest
         $api->count();
         $api->item();
         $state = $router->resolve('/foo/1/');
-        $found = json_decode("$state", true);
+        $found = json_decode($state(), true);
         $this->assertEquals('bar', $found['content']);
         $_POST = ['action' => 'update', 'data' => ['content' => 'boo']];
+        $_SERVER['REQUEST_METHOD'] = 'POST';
         $state = $router->resolve('/foo/1/', 'POST');
-        $found = json_decode("$state", true);
+        $found = json_decode($state(), true);
         $this->assertEquals('boo', $found['content']);
         $_POST = ['action' => 'delete'];
         $state = $router->resolve('/foo/1/', 'POST');
-        $state->run();
+        echo $state();
+        $_SERVER['REQUEST_METHOD'] = 'GET';
         $state = $router->resolve('/foo/count/');
-        $found = json_decode("$state", true);
+        $found = json_decode($state(), true);
         $this->assertEquals(3, $found['count']);
     }
 }
