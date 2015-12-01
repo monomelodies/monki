482c985bad8b603e2df8dc26f31e0271536ef707
Marijn Ophorst <marijn@sensimedia.nl>
2015-12-01 09:47:30 +0100

    implement stage interface and return psr-7 compatible responses

--- src/Api.php
+++ src/Api.php
@@ -2,13 +2,16 @@
 
 namespace Monki;
 
+use StdClass;
 use PDO;
 use PDOException;
 use Reroute\Router;
 use Monki\Endpoint\Item;
 use Monki\Endpoint\Browse;
-use Psr\Http\Message\RequestInterface;
-use Zend\Diactoros\Response\HtmlResponse;
+use Monki\Response\JsonResponse;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response\EmptyResponse;
+use League\Pipeline\StageInterface;
 
 $recurse = function (array &$data) use (&$recurse) {
     foreach ($data as &$value) {
@@ -27,54 +30,15 @@ $recurse = function (array &$data) use (&$recurse) {
 
 $recurse($_POST);
 
-class Api
+class Api implements StageInterface
 {
     protected $adapter;
     protected $router;
-    protected $status = [
-        401 => 'Unauthorized',
-        403 => 'Forbidden',
-        404 => 'Not found',
-        405 => 'Method not allowed',
-        500 => 'Internal server error',
-    ];
 
-    public function __construct(PDO $adapter, Router $router)
+    public function __construct(PDO $adapter, $url = '/')
     {
         $this->adapter = $adapter;
-        $this->router = $router;
-    }
-
-    public function handle($controller, $table, $verb)
-    {
-        if ($verb == 'POST') {
-            if (!isset($_POST['action'])) {
-                $_POST['action'] = 'create';
-            }
-            if (method_exists($controller, $_POST['action'])) {
-                $id = $controller->{$_POST['action']}(
-                    isset($_POST['data']) ? $_POST['data'] : []
-                );
-            }
-            if ($_POST['action'] == 'create') {
-                if ($id == 0) {
-                    return json_encode(new \StdClass);
-                }
-                $stmt = $this->adapter->prepare(sprintf(
-                    "SELECT * FROM %s WHERE id = ?",
-                    $table
-                ));
-                try {
-                    $stmt->execute([$id]);
-                    $item = $stmt->fetch(PDO::FETCH_ASSOC);
-                    return (new HtmlResponse(json_encode($item)))
-                        ->withHeader('Content-Type', 'application/json');
-                } catch (PDOException $e) {
-                    return $this->error(500);
-                }
-            }
-        }
-        return new Browse\View($this->adapter, $table);
+        $this->router = new Router($url);
     }
 
     public function browse(callable $validate = null)
@@ -85,12 +49,38 @@ class Api
              ->pipe($validate)
              ->then(
                 'monki-browse',
-                function ($table, RequestInterface $request) {
-                    return $this->handle(
-                        new Item\Controller($this->adapter, $table),
-                        $table,
-                        $request->getMethod()
-                    );
+                function ($table, ServerRequestInterface $request) {
+                    if ($request->getMethod() == 'POST') {
+                        if (!isset($_POST['action'])) {
+                            $_POST['action'] = 'create';
+                        }
+                        $controller = new Item\Controller(
+                            $this->adapter,
+                            $table
+                        );
+                        if (method_exists($controller, $_POST['action'])) {
+                            $id = $controller->{$_POST['action']}(
+                                isset($_POST['data']) ? $_POST['data'] : []
+                            );
+                        }
+                        if ($_POST['action'] == 'create') {
+                            if ($id == 0) {
+                                return json_encode(new StdClass);
+                            }
+                            $stmt = $this->adapter->prepare(sprintf(
+                                "SELECT * FROM %s WHERE id = ?",
+                                $table
+                            ));
+                            try {
+                                $stmt->execute([$id]);
+                                $item = $stmt->fetch(PDO::FETCH_ASSOC);
+                                return new JsonResponse($item);
+                            } catch (PDOException $e) {
+                                return new EmptyResponse(500);
+                            }
+                        }
+                    }
+                    return new Browse\View($this->adapter, $table);
                  });
     }
 
@@ -101,6 +91,7 @@ class Api
              ->when("/(?'table'\w+)/count/")
              ->pipe($validate)
              ->then('monki-count', function ($table) {
+                var_dump($table);
                 return new Item\Cnt($this->adapter, $table);
              });
     }
@@ -113,7 +104,7 @@ class Api
              ->pipe($validate)
              ->then(
                 'monki-item',
-                function ($table, $id, RequestInterface $request) {
+                function ($table, $id, ServerRequestInterface $request) {
                     $stmt = $this->adapter->prepare(sprintf(
                         "SELECT * FROM %s WHERE id = ?",
                         $table
@@ -139,18 +130,15 @@ class Api
                                 isset($_POST['data']) ? $_POST['data'] : null
                             );
                             $stmt->execute([$id]);
+                            if ($_POST['action'] == 'delete') {
+                                return new EmptyResponse(200);
+                            }
                             $item = $stmt->fetch(PDO::FETCH_ASSOC);
                         } else {
-                            header(
-                                "HTTP/1.1 405 {$this->status[405]}",
-                                true,
-                                405
-                            );
-                            return;
+                            return new EmptyResponse(405);
                         }
                     }
-                    return (new HtmlResponse(json_encode($item)))
-                        ->withHeader('Content-type', 'application/json');
+                    return new JsonResponse($item);
                  }
             );
     }
@@ -161,6 +149,19 @@ class Api
             $validate = function ($payload) { return $payload; };
         }
         return new Stage($validate);
+    }
+
+    public function process($payload = null)
+    {
+        return $this($payload);
+    }
+
+    public function __invoke($payload = null)
+    {
+        if (isset($payload) && !($payload instanceof ServerRequestInterface)) {
+            return $payload;
+        }
+        return $this->router->process($payload);
     }
 }
 
