c194c573662b3bc1f28775d0979be0b3859c5e7d
Marijn Ophorst <marijn@sensimedia.nl>
2015-12-06 10:34:38 +0100

    api docs

--- src/Response/JsonResponse.php
+++ src/Response/JsonResponse.php
@@ -4,8 +4,16 @@ namespace Monki\Response;
 
 use Zend\Diactoros\Response\HtmlResponse;
 
+/**
+ * Simple extension to HtmlResponse to easily return Json responses.
+ */
 class JsonResponse extends HtmlResponse
 {
+    /**
+     * @param mixed $data The data to respond with. Will be json_encoded first.
+     * @param int $status Status code, typically 200 (the default).
+     * @param array $headers Optional custom headers.
+     */
     public function __construct($data, $status = 200, array $headers = [])
     {
         $json = json_encode($data);
