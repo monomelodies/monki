6573bf0675227e4bfd96117068bf6f6c6a58b68c
Marijn Ophorst <marijn@monomelodies.nl>
2018-01-11 08:58:08 +0100

    add docblocks and type hints

--- src/Handler/Crud.php
+++ src/Handler/Crud.php
@@ -7,12 +7,25 @@ use Monomelodies\Monki\Response\JsonResponse;
 
 abstract class Crud
 {
-    protected function jsonResponse($data, $code = 200)
+    /**
+     * Return a JsonResponse.
+     *
+     * @param mixed $data
+     * @param int $code HTTP response code, defaults to 200 (OK).
+     * @return Monomelodies\Monki\Response\JsonResponse
+     */
+    protected function jsonResponse($data, $code = 200) : JsonResponse
     {
         return new JsonResponse($data, $code);
     }
 
-    protected function emptyResponse($code = 200)
+    /**
+     * Return an empty reponse.
+     *
+     * @param int $code HTTP response code, defaults to 200 (OK).
+     * @return Zend\Diactoros\Response\EmptyResponse
+     */
+    protected function emptyResponse($code = 200) : EmptyResponse
     {
         return new EmptyResponse($code);
     }
