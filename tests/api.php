d1baaeef40e55e08c79b9975638a9fa4135d9a3e
Marijn Ophorst <marijn@monomelodies.nl>
2019-12-02 17:29:27 +0100

    rename and start fixing

--- tests/api.php
+++ tests/api.php
@@ -1,24 +1,16 @@
 <?php
 
-namespace Monomelodies\Monki\Tests;
-
 use Monomelodies\Monki\Api;
 use Monomelodies\Monki\Handler\Crud;
 use Monolyth\Reroute\Router;
 use Zend\Diactoros\ServerRequestFactory;
-use PHPUnit_Extensions_Database_TestCase;
 use Quibble\Sqlite\Adapter;
 use Quibble\Query\Buildable;
 use Psr\Http\Message\RequestInterface;
 
-class ApiTest
-{
-    static private $pdo = null;
-    private $api;
-    
-    public function __construct()
-    {
-        $this->handler = new class extends Crud {
+return function () : Generator {
+    $this->beforeEach(function () use (&$handler) {
+        $handler = new class extends Crud {
 
             public function browse()
             {
@@ -50,40 +42,37 @@ class ApiTest
             }
 
         };
-    }
+    });
 
-    public function __wakeup()
-    {
+    $this->beforeEach(function () {
         $_SERVER['HTTP_HOST'] = 'localhost';
         $_SERVER['REQUEST_METHOD'] = 'GET';
-    }
+    });
 
     /**
-     * We can retrieve a list of json encoded items using browse {?}. We can
-     * post to create a new item {?}.
+     * We can retrieve a list of json encoded items using browse. We can
+     * post to create a new item.
      */
-    public function testBrowse()
-    {
+    yield function () {
         $api = new Api('/');
         $api->crud('/foo/', $this->handler);
         $_SERVER['REQUEST_URI'] = '/foo/';
         $response = $api(ServerRequestFactory::fromGlobals());
         $found = json_decode($response->getBody(), true);
-        yield assert(count($found) == 4);
+        assert(count($found) == 4);
         $_POST = ['content' => 'whee'];
         $_SERVER['REQUEST_METHOD'] = 'POST';
         $_SERVER['REQUEST_URI'] = '/foo/';
         $response = $api(ServerRequestFactory::fromGlobals());
         $found = json_decode($response->getBody(), true);
-        yield assert($found['content'] == 'whee');
-    }
+        assert($found['content'] == 'whee');
+    };
 
     /**
      * We can call our custom PUT method {?} and it gives the expected
      * custom result {?}.
      */
-    public function testCustom()
-    {
+    yield function () {
         $api = new Api('/');
         $api->crud('/foo/', $this->handler);
         $_SERVER['REQUEST_URI'] = '/foo/1/custom/';
@@ -92,7 +81,7 @@ class ApiTest
         if ($response) {
             $found = json_decode($response->getBody(), true);
         }
-        yield assert($found['type'] == 'custom');
-    }
-}
+        assert($found['type'] == 'custom');
+    };
+};
 
